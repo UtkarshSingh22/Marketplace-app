6b9ab6c3bac419f0ba6be2fbdbd0695f23c1645a
Utkarsh Singh <sutkarsh453@gmail.com>
2022-10-08 13:40:08 +0530

    Import dotenv for environment variables

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import authRouter from "./routes/auth";
+import morgan from 'morgan'
+require("dotenv").config();
 
 const app = express();
 
 app.use("/api", authRouter);
 
-app.listen(3000);
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
