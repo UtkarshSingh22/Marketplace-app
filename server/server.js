61b74cde81c548953a5efc0152a4ab491d04a1b8
Utkarsh Singh <sutkarsh453@gmail.com>
2022-10-08 14:23:57 +0530

    Connect mongodb to the app

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 import express from "express";
 import authRouter from "./routes/auth";
-import morgan from 'morgan'
+import cors from "cors";
+import mongoose from "mongoose";
+const morgan = require("morgan");
 require("dotenv").config();
 
 const app = express();
 
+//db connection
+mongoose.connect(process.env.DATABASE, {}).then(client => {
+    console.log('db connected')
+}).catch((err) => console.log('db connection failed: ' + err ))
+
+//middlewares
+app.use(morgan("dev"));
+app.use(cors());
+
+//routers
 app.use("/api", authRouter);
 
 const port = process.env.PORT || 8000;
