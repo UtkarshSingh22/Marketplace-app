1f72b10c35302dd3b0743bb8ea84d7dfb3a209f5
Utkarsh Singh <sutkarsh453@gmail.com>
2022-10-08 16:55:21 +0530

    Add form to register

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,5 +1,57 @@
+import { Fragment, useState } from "react";
+
 const Register = () => {
-    return <div className="registerPage">Register Page</div>;
+    const [nameInput, setNameInput] = useState("");
+    const [emailInput, setEmailInput] = useState("");
+    const [passwordInput, setPasswordInput] = useState("");
+
+    const nameInputHandler = (event) => {
+        setNameInput(event.target.value);
+    };
+
+    const emailInputHandler = (event) => {
+        setEmailInput(event.target.value);
+    };
+
+    const passwordInputHandler = (event) => {
+        setPasswordInput(event.target.value);
+    };
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        console.log(nameInput, emailInput, passwordInput);
+    };
+
+    return (
+        <Fragment className="registerPage">
+            <h1>Sign-up</h1>
+            <form onSubmit={formSubmitHandler}>
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    onChange={nameInputHandler}
+                    value={nameInput}
+                />
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    onChange={emailInputHandler}
+                    value={emailInput}
+                />
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    onChange={passwordInputHandler}
+                    value={passwordInput}
+                />
+
+                <button type="submit">Register</button>
+            </form>
+        </Fragment>
+    );
 };
 
 export default Register;
