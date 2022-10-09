52a270c62778c01aeb648d1e470b26e68578d355
Utkarsh Singh <sutkarsh453@gmail.com>
2022-10-09 15:27:23 +0530

    Add compare password method in user schema

--- server/models/user.js
+++ server/models/user.js
@@ -46,4 +46,15 @@ userSchema.pre("save", function (next) {
     }
 });
 
+userSchema.methods.comparePassword = function (password, next) {
+    bcrypt.compare(password, this.password, function (err, match) {
+        if (err) {
+            console.log("Compare password err", err);
+            return next(err, false);
+        }
+        console.log("Match password", match);
+        return next(null, match);
+    });
+};
+
 export default mongoose.model("User", userSchema);
