33ad707ec46f12fe9fd27a4f9f1cea6b9f67b4d0
Utkarsh Singh <sutkarsh453@gmail.com>
2022-10-09 15:52:46 +0530

    Add login controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,12 +6,13 @@ exports.register = async (req, res, next) => {
     if (!name) {
         return res.status(400).send("Name is required");
     }
+    if (!email) {
+        return res.status(400).send("Email is required");
+    }
     if (!password || password.length < 6 || password.length > 64) {
         return res
             .status(400)
-            .send(
-                "Password is required and should be of min 6 and max 64 characters"
-            );
+            .send("Password is required and should be of 6 - 64 characters");
     }
     let userExist = await User.findOne({ email }).exec();
     if (userExist) {
@@ -28,5 +29,36 @@ exports.register = async (req, res, next) => {
     } catch (err) {
         console.log("Create user failed", err);
         return res.status(400).send("Error. Try again.");
+    }
+};
+
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+        return res.status(400).send("Email is required");
+    }
+    if (!password || password.length < 6 || password.length > 64) {
+        return res
+            .status(400)
+            .send("Password is required and should be of 6 - 64 characters");
+    }
+
+    try {
+        const user = await User.findOne({ email: email }).exec();
+
+        if (!user) {
+            return res
+                .status(400)
+                .send("No user found with this email. Please register first.");
+        }
+        user.comparePassword(password, (err, match) => {
+            if (!match || err) {
+                return res.status(400).send("You entered the wrong password.");
+            }
+        });
+    } catch (err) {
+        console.log("Login error", err);
+        res.status(400).send("Sign in failed.");
     }
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/auth");
 const router = express.Router();
 
 router.post("/register", authController.register);
+router.post('/login', authController.login)
 
 export default router;
