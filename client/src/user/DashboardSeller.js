a90e362b0d399eb78f63ac5bd81dc92dca1428c2
Utkarsh Singh <sutkarsh453@gmail.com>
2022-10-10 21:08:34 +0530

    Add content for new hotel page

--- client/src/user/DashboardSeller.js
+++ client/src/user/DashboardSeller.js
@@ -2,19 +2,51 @@ import { Fragment } from "react";
 import ConnectNav from "../components/ConnectNav";
 import DashboardNav from "../components/DashboardNav";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const DashboardSeller = () => {
-    return (
-        <Fragment>
-            <ConnectNav />
+    const { auth } = useSelector((state) => ({ ...state }));
 
-            <DashboardNav />
+    const connected = () => (
+        <Fragment>
             <div>
                 <h2>Your Hotels</h2>
             </div>
             <div>
                 <Link to="/hotels/new">+ Add New</Link>
             </div>
+        </Fragment>
+    );
+
+    const notConnected = () => (
+        <Fragment>
+            <div>
+                <h2>Connect with stripe</h2>
+            </div>
+            <h4>Setup payouts to post hotel rooms</h4>
+            <p>
+                MERN partners with stripe to transfer earnings to your bank
+                account
+            </p>
+            <button>Setup Payouts</button>
+            <p>
+                You'll be redirected to Stripe to complete the onboarding
+                process.
+            </p>
+        </Fragment>
+    );
+
+    return (
+        <Fragment>
+            <ConnectNav />
+
+            <DashboardNav />
+            {auth &&
+            auth.user &&
+            auth.user.stripe_seller &&
+            auth.user.stripe_seller.charges_enabled
+                ? connected()
+                : notConnected()}
         </Fragment>
     );
 };
