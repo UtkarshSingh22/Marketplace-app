d789088af15acb865cc05d82512d65713a25e3ce
Utkarsh Singh <sutkarsh453@gmail.com>
2022-10-10 21:46:56 +0530

    Send request to backend for connecting with stripe

--- client/src/user/DashboardSeller.js
+++ client/src/user/DashboardSeller.js
@@ -1,11 +1,26 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import ConnectNav from "../components/ConnectNav";
 import DashboardNav from "../components/DashboardNav";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { createConnectAccount } from "../actions/stripe";
+import { toast } from "react-toastify";
 
 const DashboardSeller = () => {
     const { auth } = useSelector((state) => ({ ...state }));
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const payoutClickHandler = async () => {
+        setIsLoading(true);
+        try {
+            let response = await createConnectAccount(auth.token);
+            setIsLoading(false);
+        } catch (err) {
+            toast.error("Stripe connect failed, Try again.");
+            setIsLoading(false);
+        }
+    };
 
     const connected = () => (
         <Fragment>
@@ -23,12 +38,15 @@ const DashboardSeller = () => {
             <div>
                 <h2>Connect with stripe</h2>
             </div>
+            {/* home icon */}
             <h4>Setup payouts to post hotel rooms</h4>
             <p>
                 MERN partners with stripe to transfer earnings to your bank
                 account
             </p>
-            <button>Setup Payouts</button>
+            <button disabled={isLoading} onClick={payoutClickHandler}>
+                {isLoading ? "Processing..." : "Setup Payouts"}
+            </button>
             <p>
                 You'll be redirected to Stripe to complete the onboarding
                 process.
