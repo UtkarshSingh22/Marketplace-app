f0c468aa76c8ca98a2c7a43dd8d8cb4ad7e8d42a
Utkarsh Singh <sutkarsh453@gmail.com>
2022-10-14 17:51:12 +0530

    Create new stripe account for user

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -1,3 +1,14 @@
+import User from "../models/user";
+require("dotenv").config();
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_SECRET);
+
 exports.createConnectAccount = async (req, res, next) => {
-    console.log(req.auth);
+    // req.auth._id,
+    // console.log(req.headers.Authorization);
+    const user = await User.findById(req.auth._id).exec();
+
+    const account = await stripe.accounts.create({ type: "express" });
+
+    console.log(account);
 };
