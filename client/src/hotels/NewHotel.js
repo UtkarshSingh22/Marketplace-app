cfc0e54381dc65a98c3b7d58db73058451fe1061
Utkarsh Singh <sutkarsh453@gmail.com>
2022-10-15 18:15:59 +0530

    Add form for adding hotel

--- client/src/hotels/NewHotel.js
+++ client/src/hotels/NewHotel.js
@@ -1,5 +1,49 @@
+import AlgoliaPlaces from "algolia-places-react";
+import { Fragment, useState } from "react";
+
 const NewHotel = () => {
-    return <div>Add a new hotel</div>;
+    const [values, setValues] = useState({
+        title: "",
+        content: "",
+        location: "",
+        image: "",
+        price: "",
+        from: "",
+        to: "",
+        bed: "",
+    });
+
+    const { title, content, location, image, price, from, to, bed } = values;
+
+    const formSubmitHandler = (event) => {};
+
+    const imageChangeHandler = (event) => {};
+
+    const changeHandler = (event) => {};
+
+    const hotelForm = () => {
+        return (
+            <form onSubmit={formSubmitHandler}>
+                <label>
+                    Image
+                    <input
+                        type="file"
+                        name="image"
+                        onChange={imageChangeHandler}
+                        accept="image/*"
+                        hidden
+                    />
+                </label>
+            </form>
+        );
+    };
+
+    return (
+        <Fragment>
+            <h2>Add Hotel</h2>
+            {hotelForm()}
+        </Fragment>
+    );
 };
 
 export default NewHotel;
