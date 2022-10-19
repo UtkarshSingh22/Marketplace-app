3cf0865dd2d47981c2c033545b5eda96c208f6f7
Utkarsh Singh <sutkarsh453@gmail.com>
2022-10-19 21:04:58 +0530

    Add search result component

--- client/src/hotels/SearchResults.js
+++ client/src/hotels/SearchResults.js
@@ -1,6 +1,26 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Search from "../components/forms/Search";
 
 const SearchResults = () => {
+    const [searchParams] = useSearchParams();
+
+    const [location, setLocation] = useState("");
+    const [fromDate, setFromDate] = useState("");
+    const [toDate, setToDate] = useState("");
+    const [bed, setBed] = useState("");
+    const [hotels, setHotels] = useState([]);
+
+    useEffect(() => {
+        const location = searchParams.get("location");
+        const fromDate = searchParams.get("fromDate");
+        const toDate = searchParams.get("toDate");
+        const bed = searchParams.get("bed");
+
+        console.table({ location, fromDate, toDate, bed });
+    }, [window.location.search]);
+
     return (
         <Fragment>
             <div>search result</div>
